788083698e5ba2c003c531401eda1e5115d276c4
boomboom0202 <117350278+boomboom0202@users.noreply.github.com>
2025-08-12 17:16:31 +0500

    Update views.py

--- lms/djangoapps/univerapi/views.py
+++ lms/djangoapps/univerapi/views.py
@@ -36,7 +36,7 @@ class UniverView(GenericAPIView):
     @method_decorator(csrf_protect)
     def post(self, request):
         return HttpResponse('post')
-   
+
         auth = request.POST.get('auth')
         encoded_jwt = request.GET.get('auth')
         decoded = jwt.decode(encoded_jwt, "$ecRet@3#$2958GPIs!1", algorithms=["HS256"])
@@ -46,13 +46,13 @@ class UniverView(GenericAPIView):
         created_user_email = ''
         try:
             exist_user = Univeruser.objects.get(univer_id=decoded['uname'])
-            
+
             return HttpResponse('user exist')
 
         except:
-	    surname, name, gender, stage, birth_year = decode_token_and_fetch_profile(encoded_jwt)
-	    created_user = decoded['uname'].replace(".", "_") + str(random.randint(100, 999))
-            created_user_email = decoded['uname'].replace(".", "_")+'@kaznu.edu.kz' 
+            surname, name, gender, stage, birth_year = decode_token_and_fetch_profile(encoded_jwt)
+            created_user = decoded['uname'].replace(".", "_") + str(random.randint(100, 999))
+            created_user_email = decoded['uname'].replace(".", "_")+'@kaznu.edu.kz'
             multipart_form_data = {
                 'email': (None, created_user_email),
                 'password': (None, decoded['upwd'][:75]),
@@ -62,10 +62,10 @@ class UniverView(GenericAPIView):
                 'gender': (None, gender),
                 'year_of_birth': (None, birth_year),
                 'honor_code': (None, 'true'),
-		'country': (None, 'KZ'),
-		'goals': (None, 'education'),
-		'surname': (None, surname),
-		'mailing_address': (None, 'Kaznu')
+                'country': (None, 'KZ'),
+                'goals': (None, 'education'),
+                'surname': (None, surname),
+                'mailing_address': (None, 'Kaznu')
             }
             client = requests.session()
             client.get('http://192.168.199.128', verify=False)
@@ -76,31 +76,31 @@ class UniverView(GenericAPIView):
                'X-CSRFToken': csrftoken,
                'Cookie':coky
             }
-       
+
             response = requests.post('http://192.168.199.128:8181/user_api/v1/account/registration/', data=login_data, files=multipart_form_data, headers=headers, verify=False)
 
             json_data = json.loads(response.text)
-            
+
             if ( ('success' in json_data) and (json_data['success'] == True)):
                 created_user_str = str(created_user)
-                
+
                 exist_user = None
                 i = 0
                 while i < 60:
-                    try:  
+                    try:
                         exist_user = User.objects.get(username=created_user_str)
                         User.refresh_from_db()
                         break
                     except:
-                        i += 1    
+                        i += 1
                         time.sleep(3)
-                        
-                out_html=''    
-                query = """SELECT id, username FROM auth_user where username LIKE '""" + "%%" + "hakymova" + "%%" + """'""" 
+
+                out_html=''
+                query = """SELECT id, username FROM auth_user where username LIKE '""" + "%%" + "hakymova" + "%%" + """'"""
                 query2 = """SELECT id, username FROM auth_user where username = '""" + created_user_str + """'"""
 
 
-                return HttpResponse(repr(exist_user)) 
+                return HttpResponse(repr(exist_user))
 
                 time.sleep(10)
                 cursor = connection.cursor()
@@ -109,24 +109,24 @@ class UniverView(GenericAPIView):
                 try:
                     cursor.execute(str(query2))
                     row = cursor.fetchall()
-                    return HttpResponse(repr(row)) 
+                    return HttpResponse(repr(row))
                 except Exception as e:
                     cursor.close
-                    return HttpResponse('error =' + str(e)) 
+                    return HttpResponse('error =' + str(e))
 
-                return HttpResponse(repr(cursor)) 
+                return HttpResponse(repr(cursor))
             else:
-                return HttpResponse('no registration')    
-            
+                return HttpResponse('no registration')
+
             return HttpResponse((json_data['success']))
 
 
         return HttpResponse(repr(exist_user))
 
         multipart_form_data = {
-	        'email': (None, 'dl@kaznu.kz'),
-	        'password': (None, 'DlGao3wo'),
-	        'remember': (None, 'true')
+                'email': (None, 'dl@kaznu.kz'),
+                'password': (None, 'DlGao3wo'),
+                'remember': (None, 'true')
         }
         client = requests.session()
         client.get('http://192.168.199.128')
@@ -137,11 +137,11 @@ class UniverView(GenericAPIView):
             'X-CSRFToken': csrftoken,
             'Cookie':coky
         }
-       
+
         response = requests.post('http://192.168.199.128/user_api/v1/account/login_session/', data=login_data, files=multipart_form_data, headers=headers, verify = False)
 
         headers2 = response.headers
-        
+
         csrfToken = re.search(r'csrftoken=(\w+);', headers2['Set-Cookie'])
         sessionid = re.search(r'sessionid="(\S+)";', headers2['Set-Cookie'])
 
@@ -172,7 +172,7 @@ class UniverView(GenericAPIView):
 
     @method_decorator(ensure_csrf_cookie)
     def get(self, request):
-        current_site = 'https://open.kaznu.kz'        
+        current_site = 'https://open.kaznu.kz'
 
         auth = request.POST.get('auth')
         encoded_jwt = request.GET.get('auth')
@@ -182,12 +182,12 @@ class UniverView(GenericAPIView):
 
         try:
             exist_user = Univeruser.objects.get(univer_id=decoded['uname'])
-            
+
             return render(request, 'univerapi/redirect.html')
-	
+
         except:
-	    surname, name, gender, stage, birth_year = decode_token_and_fetch_profile(encoded_jwt)
-	    created_user = decoded['uname'].replace(".", "_") + str(random.randint(100, 999))
+            surname, name, gender, stage, birth_year = decode_token_and_fetch_profile(encoded_jwt)
+            created_user = decoded['uname'].replace(".", "_") + str(random.randint(100, 999))
 
             created_user_email = decoded['uname'].replace(".", "_")+'@kaznu.edu.kz'
             multipart_form_data = {
@@ -199,10 +199,10 @@ class UniverView(GenericAPIView):
                 'gender': (None, gender),
                 'year_of_birth': (None, birth_year),
                 'honor_code': (None, 'true'),
-		'country': (None , 'KZ'),
-		'surname': (None, surname),
-		'goals': (None, 'education'),
-		'mailing_address': (None, 'Kaznu')
+                'country': (None , 'KZ'),
+                'surname': (None, surname),
+                'goals': (None, 'education'),
+                'mailing_address': (None, 'Kaznu')
             }
             client = requests.session()
             client.get(current_site, verify = False)
@@ -213,49 +213,49 @@ class UniverView(GenericAPIView):
                'X-CSRFToken': csrftoken,
                'Cookie':coky
             }
-           
+
             count_try = 0
             while True:
                 response = requests.post(current_site + '/user_api/v1/account/registration/', data=login_data, files=multipart_form_data, headers=headers, verify = False)
                 json_data = json.loads(response.text)
                 if ( ('success' in json_data) and (json_data['success'] == True)):
                     break
-                if ( ('username' in json_data) ):    
-                    if "existing account" in str(json_data): 
-                       
+                if ( ('username' in json_data) ):
+                    if "existing account" in str(json_data):
+
 
                         return render(request, 'univerapi/redirect.html')
-                time.sleep(3)   
+                time.sleep(3)
                 count_try += 1
                 if (count_try == 20):
-                    return JsonResponse(json_data)       
-            
+                    return JsonResponse(json_data)
+
             if ( ('success' in json_data) and (json_data['success'] == True)):
                 new_user = Univeruser()
                 new_user.edx_username = created_user
                 new_user.edx_id=0
                 new_user.edx_pass='0'
                 new_user.univer_id = decoded['uname']
-                new_user.save()                
+                new_user.save()
                 return render(request, 'univerapi/redirect.html')
                 created_user_str = str(created_user)
-                
+
                 exist_user = None
                 i = 0
                 while i < 60:
-                    try:  
+                    try:
                         exist_user = User.objects.get(username=created_user_str)
                         User.refresh_from_db()
                         break
                     except:
-                        i += 1    
+                        i += 1
                         time.sleep(3)
-                        
-                out_html=''    
-                query = """SELECT id, username FROM auth_user where username LIKE '""" + "%%" + "hakymova" + "%%" + """'""" 
+
+                out_html=''
+                query = """SELECT id, username FROM auth_user where username LIKE '""" + "%%" + "hakymova" + "%%" + """'"""
                 query2 = """SELECT id, username FROM auth_user where username = '""" + created_user_str + """'"""
 
-                return HttpResponse(repr(exist_user)) 
+                return HttpResponse(repr(exist_user))
 
                 time.sleep(10)
                 cursor = connection.cursor()
@@ -264,18 +264,18 @@ class UniverView(GenericAPIView):
                 try:
                     cursor.execute(str(query2))
                     row = cursor.fetchall()
-                    return HttpResponse(repr(row)) 
+                    return HttpResponse(repr(row))
                 except Exception as e:
                     cursor.close
-                    return HttpResponse('error =' + str(e)) 
+                    return HttpResponse('error =' + str(e))
 
-                return HttpResponse(repr(cursor))     
+                return HttpResponse(repr(cursor))
             else:
                 return HttpResponse('no registration')
 
 
 def authview(request):
-	return render(request, 'univerapi/index.html')
+        return render(request, 'univerapi/index.html')
 
 
 def authview_check(request):
@@ -332,11 +332,11 @@ def decode_token_and_fetch_profile(token, secret_key='$ecRet@3#$2958GPIs!1'):
             student_data = student_resp.json()
         except:
             pass
-    
+
     # Если данные студента получены успешно
     if student_data and student_data.get('code') == 0 and 'data' in student_data:
         info_list, personal_list = student_data['data']
-        
+
         def find_value(lst, key):
             for d in lst:
                 if key in d and ':' in d[key]:
@@ -345,7 +345,7 @@ def decode_token_and_fetch_profile(token, secret_key='$ecRet@3#$2958GPIs!1'):
 
         surname = find_value(personal_list, 'sname')
         name    = find_value(personal_list, 'name')
-        
+
         raw_sex = find_value(personal_list, 'sex') or ''
         if isinstance(raw_sex, bytes):
             raw_sex = raw_sex.decode('utf-8')
@@ -382,7 +382,7 @@ def decode_token_and_fetch_profile(token, secret_key='$ecRet@3#$2958GPIs!1'):
         raise SystemExit("teacher/profile 500:\n{0}".format(teacher_resp.text))
     teacher_resp.raise_for_status()
     teacher_data = teacher_resp.json()
-    
+
     if teacher_data.get('code') != 0 or 'data' not in teacher_data:
         raise SystemExit("Не удалось получить данные ни студента, ни преподавателя")
 
@@ -392,7 +392,7 @@ def decode_token_and_fetch_profile(token, secret_key='$ecRet@3#$2958GPIs!1'):
     name = teacher_profile.get('name', '')
     gender = 'o'  # По умолчанию, если поле отсутствует
     stage = 'none'  # Для преподавателя ступень не применима
-    
+
     # Извлечение года рождения из даты (формат "дд.мм.гггг")
     birth_year = None
     if 'dateOfBirth' in teacher_profile:
