1a9b7c3c8dc3d549e6a9dd5c634c331bf2e63ad2
boomboom0202 <abylaikhan.suev.02@gmail.com>
2025-09-24 14:20:54 +0500

    Обновил нужные строки

--- lms/djangoapps/news/admin.py
+++ lms/djangoapps/news/admin.py
@@ -1,18 +1,7 @@
 from django.contrib import admin
-from django import forms
 from .models import News
-
-class NewsAdminForm(forms.ModelForm):
-    content = forms.CharField(widget=forms.Textarea(attrs={'rows': 30, 'cols': 120}))
-    
-    class Meta:
-        model = News
-        fields = '__all__'
 
 @admin.register(News)
 class NewsAdmin(admin.ModelAdmin):
-    form = NewsAdminForm
-    list_display = ('title', 'created_at', 'updated_at')
+    list_display = ('title', 'created_at')
     search_fields = ('title', 'content')
-    list_filter = ('created_at',)
-    date_hierarchy = 'created_at'
