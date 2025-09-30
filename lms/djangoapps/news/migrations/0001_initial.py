e0420f4fe09641b8778bef8a4f2f9155ea820ed9
boomboom0202 <117350278+boomboom0202@users.noreply.github.com>
2025-09-30 16:47:58 +0500

    Update 0001_initial.py

--- lms/djangoapps/news/migrations/0001_initial.py
+++ lms/djangoapps/news/migrations/0001_initial.py
@@ -14,10 +14,10 @@ class Migration(migrations.Migration):
         migrations.CreateModel(
             name='News',
             fields=[
-                ('id', models.AutoField(auto_created=True, primary_key=True, serialize=False, verbose_>
+                ('id', models.AutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')),
                 ('title', models.CharField(max_length=200, verbose_name='Заголовок')),
                 ('content', models.TextField(verbose_name='Содержание')),
-                ('image', models.ImageField(blank=True, null=True, upload_to='articles/', verbose_name>
+                ('image', models.ImageField(blank=True, null=True, upload_to='articles/', verbose_name='Изображение')),
                 ('created_at', models.DateTimeField(auto_now_add=True, verbose_name='Дата создания')),
                 ('updated_at', models.DateTimeField(auto_now=True, verbose_name='Дата обновления')),
             ],
@@ -28,4 +28,3 @@ class Migration(migrations.Migration):
             },
         ),
     ]
-
